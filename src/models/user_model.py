012b955e68ec2bd50d305dd4a85b05d3ecf28561
Erne <110843039+ernestasb@users.noreply.github.com>
2023-08-12 01:08:15 +0300

    User model init

--- src/models/user_model.py
+++ src/models/user_model.py
@@ -1,1 +1,26 @@
+import hashlib
 
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import Session
+
+from src.database import Base
+from src.extensions import SecurityManager
+
+
+class User(Base):
+    """
+    User table, containing user information
+    """
+
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    username = Column(String(20), nullable=False)
+    password = Column(String(80), nullable=False)
+
+    async def create_user(self, session: Session):
+        # hashing password before save
+        self.password = SecurityManager.hash(hash_string=self.password)
+        session.add(self)
+        session.commit()
+        return self
